efc3236934906f0e1f16dc63ecedff37e93d6d05
luke <garcia.luc66@gmail.com>
2022-12-01 20:40:48 +0100

    Player's movements

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import './style.css'
+
 import ReactDOM from 'react-dom/client'
+
 import { Canvas } from '@react-three/fiber'
+import { KeyboardControls } from '@react-three/drei'
+
 import Experience from './Experience.js'
+
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
+  <KeyboardControls
+    map={[
+      { name: 'forward', keys: ['ArrowUp', 'z'] },
+      { name: 'backward', keys: ['ArrowDown', 's'] },
+      { name: 'leftward', keys: ['ArrowLeft', 'q'] },
+      { name: 'rightward', keys: ['ArrowRight', 'd'] },
+      { name: 'jump', keys: ['Space'] },
+    ]}
+  >
     <Canvas
-        shadows
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ 2.5, 4, 6 ]
-        } }
+      shadows
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [2.5, 4, 6],
+      }}
     >
-        <Experience />
+      <Experience />
     </Canvas>
-)
+  </KeyboardControls>
+);
