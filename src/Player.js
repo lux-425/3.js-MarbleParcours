a8cff62cc0ea72629d0379c557b65833d78b1215
luke <garcia.luc66@gmail.com>
2022-12-01 20:13:20 +0100

    Ball's player object and physics

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,12 @@
+import { RigidBody } from '@react-three/rapier';
+
 export default function Player() {
-  return <>
-  
-  </>
+  return (
+      <RigidBody colliders='ball' restitution={0.2} friction={1} position={[0,1,0]}>
+        <mesh castShadow>
+          <icosahedronGeometry args={[0.3, 1]} />
+          <meshStandardMaterial flatShading color='mediumpurple' />
+        </mesh>
+      </RigidBody>
+  );
 }
